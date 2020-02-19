21c2456a1df84633d0d5e87df02ce950244ac925
mayajoyiwabuchi <mayajoyiwabuchi@outlook.com>
2020-02-19 12:22:47 -0500

    2/19

---
+++ basicCubeInteract1/Assets/Scripts/InteractionCategory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionCategory : MonoBehaviour {
+
+    public bool categoryZero;
+    public bool categoryOne;
+    public bool categoryTwo;
+    public bool isConstraint;
+
+    public enum category { categoryZero = 0, categoryOne = 1 };
+    public category myCat;
+
+    
+}
---
+++ basicCubeInteract1/Assets/Scripts/interaction1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Valve.VR.InteractionSystem
+{
+
+    public class interaction1 : MonoBehaviour
+    {
+        public SteamVR_ActionSet activateActionSetOnAttach;
+        public GameObject target; 
+        public Material activeMaterial; 
+
+        public delegate void OnAttachedToHandDelegate(Hand hand);
+        public delegate void OnDetachedFromHandDelegate(Hand hand);
+        public event OnAttachedToHandDelegate onAttachedToHand;
+        public event OnDetachedFromHandDelegate onDetachedFromHand;
+
+        [System.NonSerialized]
+        public Hand attachedToHand;
+
+        protected virtual void OnAttachedToHand(Hand hand)
+        {
+            if (activateActionSetOnAttach != null)
+                activateActionSetOnAttach.Activate(hand.handType);
+
+            if (onAttachedToHand != null)
+            {
+                onAttachedToHand.Invoke(hand);
+            }
+
+            attachedToHand = hand;
+        }
+
+        void Update()
+        {
+            if (attachedToHand != null)
+            {
+                target.GetComponent<MeshRenderer>().material = activeMaterial;
+            }
+        }
+    }
+}
